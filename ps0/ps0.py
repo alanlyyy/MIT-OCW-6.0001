a93015de9b7d37ea105f3418e787a5ac1b288d16
alanlyyy <44736157+alanlyyy@users.noreply.github.com>
2018-11-03 16:50:40 -0700

    MIT OCW 6.0001: ps0 upload

---
+++ ps0/ps0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 12:05:20 2018
+
+@author: Alan Ly
+"""
+import numpy
+
+x = int(input('Enter a number x: '))
+y = int(input('Enter a number y: '))
+power = x**y
+print('x**y = ', power)
+log = numpy.log2(x)
+print('log(x): ', log)
